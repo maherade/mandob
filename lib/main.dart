7443063cdfd2176976022f1d5b97950843f91eda
DELL <mahmoud180280@fci.bu.edu.eg>
2023-06-12 15:40:25 +0300

    mahmoud reda

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
