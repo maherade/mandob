dcdeba7f043e0c216bf779bb8a5696a18cd57bfe
Maher Adel <74119717+maherade@users.noreply.github.com>
2023-08-03 15:59:48 +0300

    last update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -30,35 +30,4 @@ import FirebaseMessaging
     application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}import UIKit
-import Flutter
-import Firebase
-import FirebaseCore
-import FirebaseAuth
-import FirebaseMessaging
-
-@UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate , MessagingDelegate{
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    FirebaseApp.configure()
-    GeneratedPluginRegistrant.register(with: self)
-    Messaging.messaging().delegate = self
-    if #available(iOS 10.0, *) {
-        UNUserNotificationCenter.current().delegate = self
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-                options: authOptions,
-                completionHandler: {_, _ in })
-    } else {
-        let settings: UIUserNotificationSettings =
-        UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-        application.registerUserNotificationSettings(settings)
-        UIApplication.shared.registerForRemoteNotifications()
-    }
-    application.registerForRemoteNotifications()
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 }
